926b663750a85f0be9e9171d1efe3fc71e88e4ad
vhfmatks <vhfmatks@gmail.com>
2018-06-20 17:01:25 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-//var morgan = require('morgan');
+var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
@@ -12,23 +12,22 @@ var app = express();
 app.set('views', './public');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-
-var uri = process.env.MONGODB_URI || 'mongodb://localhost/ipkm-lunch' ;
-//var uri = 'mongodb://localhost/ipkm-lunch';
-var db = mongoose.connection;
-// db.on('error', console.error);
-// db.once('open', () => {
-//   console.log('connected to mongod server');
-// });
-// mongoose.connect(uri);
-//
-//app.use(morgan('dev'));
-app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+var uri = process.env.MONGODB_URI || 'mongodb://localhost/ipkm-lunch' ;
+//var uri = 'mongodb://localhost/ipkm-lunch';
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+  console.log('connected to mongod server');
+});
+mongoose.connect(uri);
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
 app.use('/use', useCtnt);
 app.use('/deposit', Deposit);
